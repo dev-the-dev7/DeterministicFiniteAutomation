90fefa13e977672901054dfb2e8ac649f44d537d
dev-the-dev7 <devinvella@gmail.com>
2020-08-27 18:35:41 -0500

    fix typo

--- src/dfa/DeterministicFiniteAutomation.java
+++ src/dfa/DeterministicFiniteAutomation.java
@@ -4,11 +4,15 @@ package dfa;
 import javax.swing.JOptionPane;
 
 /****************************
- ***Class Name: TheJosephusProblem
+ ***Class Name: DeterministicFiniteAutomation
  ***Author: Devin Vella
-***Purpose: The purpose of this class is to create a tool to solve the Josephus problem. 
+ ***Purpose: The purpose of this class is to allow the user to create a language
+ ***consisting of the letters a and b by utilizing deterministic finite automation. 
+ ***It does this by allowing the user to set the initial state, accepting state, 
+ ***and the rule used to change states. The user can then test if a word is 
+ ***accepted by the rules they set.
  **************************** 
- ***Date: 12/3/2018
+ ***Date: 3/20/2019
  ****************************/
 public class DeterministicFiniteAutomation extends javax.swing.JFrame {
 
